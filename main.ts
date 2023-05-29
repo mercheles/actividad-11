1c43ed4f80013f7b735ce2fc14ac1977ecfa3d33
mercheles <134915287+mercheles@users.noreply.github.com>
2023-05-29 09:03:30 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Cayuela")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Mercedes")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Barea")
+})
+basic.showString("Mercedes")
+basic.forever(function () {
+	
+})
